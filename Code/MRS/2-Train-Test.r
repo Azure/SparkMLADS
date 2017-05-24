cb7a480754a4afbb67cb2f750aee01ecf8634df5
inchiosa <marioinc@yahoo.com>
2017-05-24 01:35:25 +0000

    Initial commit

---
+++ Code/MRS/2-Train-Test.r
@@ -0,0 +1,182 @@
+setwd("/home/sshuser/marinch")
+source("SetComputeContext.r")
+
+# For local compute context, skip the following line
+#startRxSpark()
+
+
+################################################
+# Split out Training and Test Datasets
+################################################
+
+# split out the training data
+
+airWeatherTrainDF <- airWeatherDF %>% filter(Year == 2011) 
+airWeatherTrainDF <- airWeatherTrainDF %>% sdf_register("flightsweathertrain")
+
+# split out the testing data
+
+airWeatherTestDF <- airWeatherDF %>% filter(Year == 2012) 
+airWeatherTestDF <- airWeatherTestDF %>% sdf_register("flightsweathertest")
+
+
+# Create ScaleR data source objects
+
+colInfo <- list(
+  Year = list(type="factor"),
+  Month = list(type="factor"),
+  DayOfMonth = list(type="factor"),
+  DayOfWeek = list(type="factor"),
+  Carrier = list(type="factor"),
+  OriginAirportID = list(type="factor"),
+  DestAirportID = list(type="factor")
+)
+
+finalData <- RxHiveData(table = "flightsweather", colInfo = colInfo)
+colInfoFull <- rxCreateColInfo(finalData)
+
+trainDS <- RxHiveData(table = "flightsweathertrain", colInfo = colInfoFull)
+testDS <- RxHiveData(table = "flightsweathertest", colInfo = colInfoFull)
+
+
+################################################
+# Train and Test a Logistic Regression model
+################################################
+
+formula <- as.formula(ArrDel15 ~ Month + DayOfMonth + DayOfWeek + Carrier + OriginAirportID + 
+                        DestAirportID + CRSDepTime + CRSArrTime + RelativeHumidityOrigin + 
+                        AltimeterOrigin + DryBulbCelsiusOrigin + WindSpeedOrigin + 
+                        VisibilityOrigin + DewPointCelsiusOrigin + RelativeHumidityDest + 
+                        AltimeterDest + DryBulbCelsiusDest + WindSpeedDest + VisibilityDest + 
+                        DewPointCelsiusDest
+)
+
+# Use the scalable rxLogit() function
+
+system.time(
+logitModel <- rxLogit(formula, data = trainDS)
+) # 87 sec
+options(max.print = 10000)
+base::summary(logitModel)
+
+# Predict over test data (Logistic Regression).
+
+logitPredict <- RxXdfData(file.path(dataDir, "logitPredictSubset"))
+
+# Use the scalable rxPredict() function
+
+rxPredict(logitModel, data = testDS, outData = logitPredict,
+          extraVarsToWrite = c("ArrDel15"),
+          type = 'response', overwrite = TRUE)
+
+# Calculate ROC and Area Under the Curve (AUC).
+
+logitRoc <- rxRoc("ArrDel15", "ArrDel15_Pred", logitPredict)
+logitAuc <- rxAuc(logitRoc)
+# 0.619
+
+plot(logitRoc)
+
+save(logitModel, file = "logitModelSubset.RData")
+
+# rxFastTrees
+
+t2 <- system.time(
+  fastTreesModel <- rxFastTrees(formula, data = trainDS) #DROPS DATA IF trainDS is RxHiveData
+)
+# 13.6 sec
+
+fastTreesModel
+summary(fastTreesModel)
+
+fastTreesPredict <- RxXdfData(file.path(dataDir, "fastTreesPredictSubset"))
+
+rxPredict(fastTreesModel, data = testDS, outData = fastTreesPredict,  #DROPS DATA IF testDS is RxHiveData
+          extraVarsToWrite = c("ArrDel15"),
+          overwrite = TRUE)
+
+rxGetInfo(fastTreesPredict, getVarInfo = T, numRows = 3)
+# File name: C:\Users\marinch\OneDrive - Microsoft\Conferences\MLADS June 2017\Code\2017-05-04\delayDataLarge\fastTreesPredictSubset.xdf 
+# Number of observations: 402819 
+# Number of variables: 4 
+# Number of blocks: 200 
+# Compression type: none 
+# Variable information: 
+#   Var 1: ArrDel15, Type: integer, Low/High: (0, 1)
+# Var 2: PredictedLabel
+# 2 factor levels: 0 1
+# Var 3: Score.1, Type: numeric, Storage: float32, Low/High: (-9.5904, 6.1169)
+# Var 4: Probability.1, Type: numeric, Storage: float32, Low/High: (0.0211, 0.9203)
+# Data (3 rows starting with row 1):
+#   ArrDel15 PredictedLabel   Score.1 Probability.1
+# 1        0              0 -6.201247    0.07723664
+# 2        0              0 -6.456911    0.07025596
+# 3        0              0 -7.068470    0.05586200
+
+# Calculate ROC and Area Under the Curve (AUC).
+
+fastTreesRoc <- rxRoc("ArrDel15", "Probability.1", fastTreesPredict)
+fastTreesAuc <- rxAuc(fastTreesRoc)
+# 0.6530
+
+plot(fastTreesRoc)
+
+save(fastTreesModel, file = "fastTreesModelSubset.RData")
+
+# rxFastTreesEnsemble
+
+trainers <- list(fastTrees(), fastTrees(numTrees = 60), fastTrees(learningRate = 0.1))
+
+#rxSetComputeContext("localpar")
+#startRxSpark()
+
+fastTreesEnsembleModelTime <- system.time(
+  fastTreesEnsembleModel <- rxEnsemble(formula, data = trainDS,
+    type = "binary", trainers = trainers, replace = T)
+)
+# 64.73 sec
+
+fastTreesEnsembleModel
+summary(fastTreesEnsembleModel)
+
+fastTreesEnsemblePredict <- RxXdfData(file.path(dataDir, "fastTreesEnsemblePredictSubset"))
+
+rxPredict(fastTreesEnsembleModel, data = testDS, outData = fastTreesEnsemblePredict,
+          extraVarsToWrite = c("ArrDel15"),
+          overwrite = TRUE)
+
+rxGetInfo(fastTreesEnsemblePredict, getVarInfo = T, numRows = 3)
+# File name: C:\Users\marinch\OneDrive - Microsoft\Conferences\MLADS June 2017\Code\2017-05-04\delayDataLarge\fastTreesEnsemblePredictSubset.xdf 
+# Number of observations: 402819 
+# Number of variables: 4 
+# Number of blocks: 200 
+# Compression type: none 
+# Variable information: 
+#   Var 1: ArrDel15, Type: integer, Low/High: (0, 1)
+# Var 2: PredictedLabel
+# 2 factor levels: 0 1
+# Var 3: Score.1, Type: numeric, Storage: float32, Low/High: (-9.1441, 5.7202)
+# Var 4: Probability.1, Type: numeric, Storage: float32, Low/High: (0.0188, 0.9572)
+# Data (3 rows starting with row 1):
+#   ArrDel15 PredictedLabel   Score.1 Probability.1
+# 1        0              0 -6.185685    0.07252729
+# 2        0              0 -6.719369    0.05721389
+# 3        0              0 -7.188776    0.04630702
+
+# Calculate ROC and Area Under the Curve (AUC).
+
+fastTreesEnsembleRoc <- rxRoc("ArrDel15", "Probability.1", fastTreesEnsemblePredict)
+fastTreesEnsembleAuc <- rxAuc(fastTreesEnsembleRoc)
+# 0.6560
+
+plot(fastTreesEnsembleRoc)
+
+save(fastTreesEnsembleModel, file = "fastTreesEnsembleModelSubset.RData")
+
+#####################################
+# TODO: H2O interop
+#####################################
+
+
+# For local compute context, skip the following line
+rxSparkDisconnect(rxGetComputeContext())
---
+++ Code/MRS/SetComputeContext.r
@@ -0,0 +1,81 @@
+if(file.exists("/dsvm"))
+{
+  # Set environment variables for the Data Science VM
+  Sys.setenv(SPARK_HOME="/dsvm/tools/spark/current",
+             YARN_CONF_DIR="/opt/hadoop/current/etc/hadoop", 
+             JAVA_HOME = "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.111-1.b15.el7_2.x86_64",
+             PATH="/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/anaconda/envs/py35/bin:/dsvm/tools/cntk/cntk/bin:/usr/local/mpi/bin:/dsvm/tools/spark/current/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin:/home/remoteuser/.local/bin:/home/remoteuser/bin:/opt/hadoop/current/sbin:/opt/hadoop/current/bin"
+  )
+} else {
+  Sys.setenv(SPARK_HOME="/usr/hdp/current/spark2-client")
+}
+
+useHDFS <- TRUE
+
+if(useHDFS) {
+  
+  ################################################
+  # Use Hadoop-compatible Distributed File System
+  # N.B. Can be used with local or RxSpark compute contexts
+  ################################################
+  
+  # ADDED FOR HDINSIGHT CLUSTER:
+  #rxOptions(hdfsHost = "hdfs://mycluster") # to access the cluster's locally attached HDFS storage
+  
+  rxOptions(fileSystem = RxHdfsFileSystem())
+  
+  #dataDir <- "/user/RevoShare/remoteuser/Data"
+  dataDir <- "/marinch"
+  
+  ################################################
+  
+  if(rxOptions()$hdfsHost == "default") {
+    fullDataDir <- dataDir
+  } else {
+    fullDataDir <- paste0(rxOptions()$hdfsHost, dataDir)
+  }  
+} else {
+  
+  ################################################
+  # Use Native, Local File System
+  # N.B. Can only be used with local compute context
+  ################################################
+  
+  rxOptions(fileSystem = RxNativeFileSystem())
+  
+  dataDir <- file.path(getwd(), "delayDataLarge")
+  fullDataDir <- paste0("file://", dataDir)
+  
+  ################################################
+}
+
+################################################
+# Distributed computing using Spark
+################################################
+
+startRxSpark <- function() {
+  if (useHDFS) {
+    # When running on an HDInsight cluster,
+    # specifying numExecutors, executorCores,
+    # and executorMem is optional
+    rxSparkConnect(reset = T,
+                   consoleOutput = TRUE, 
+                   numExecutors = 4,  # FOR HDINSIGHT CLUSTER
+                   executorCores = 8, # FOR HDINSIGHT CLUSTER
+                   executorMem = "4g" # FOR HDINSIGHT CLUSTER
+    )
+  } else {
+    cat("Using local compute context to process local data.\n")
+  }
+}
+
+rxRoc <- function(...){
+  previousContext <- rxSetComputeContext(RxLocalSeq())
+  
+  # rxRoc requires local compute context
+  roc <- RevoScaleR::rxRoc(...)
+  
+  rxSetComputeContext(previousContext)
+  
+  return(roc)
+}
