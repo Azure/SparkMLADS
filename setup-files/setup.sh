ec2a04872bdc7d6c2ccf5a8adba0a2f8a00d6c2c
inchiosa <inchiosa@users.noreply.github.com>
2017-06-02 19:03:44 -0700

    Combine upgrade and setup

--- setup-files/setup.sh
+++ setup-files/setup.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Running the action script to set up the tutorial"
+echo "Running the action script to upgrade MRS and set up the tutorial"
+
+# Upgrade MRS
+
+apt-get -y remove microsoft-r-server-hadoop-9.0
+apt-get -y remove microsoft-r-server-packages-9.0
+apt-get -y remove microsoft-r-open-foreachiterators-3.3
+apt-get -y remove microsoft-r-open-intel-mkl-3.3
+apt-get -y remove microsoft-r-open-mro-3.3
+wget https://mrseagle.blob.core.windows.net/share/MRS_Linux.zip -O /tmp/MRS_Linux.zip
+cd /tmp
+unzip MRS_Linux.zip
+cd /tmp/MRS_Linux
+chmod 777 install.sh
+./install.sh -amp
+
+# Set up the tutorial
 
 cd /home/sshuser
 
